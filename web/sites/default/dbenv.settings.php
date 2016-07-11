c6878219f5f48aead65f7232b0dfffe74a492a45
Michael Prasuhn <mike@mikeyp.net>
2016-07-11 18:05:17 -0400

    Adding environment based DB config

---
+++ web/sites/default/dbenv.settings.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Integrates DB settings as parsed from environment variables.
+ */
+
+if (file_exists(dirname(DRUPAL_ROOT) . '/.env')) {
+  // Load environment
+  $dotenv = new Dotenv\Dotenv(dirname(DRUPAL_ROOT));
+  $dotenv->load();
+
+  // Require DATABASE_URL to be defined
+  $dotenv->required('DATABASE_URL')->notEmpty();
+
+  $dsn_info = parse_url(getenv('DATABASE_URL'));
+
+  $databases['default']['default'] = array (
+    'database' => ltrim($dsn_info['path'], '/'),
+    'username' => $dsn_info['user'],
+    'password' => $dsn_info['pass'],
+    'host' => $dsn_info['host'],
+    'port' => $dsn_info['port'],
+    'driver' => $dsn_info['scheme'],
+    'prefix' => '',
+    'collation' => 'utf8mb4_general_ci',
+  );
+
+}
