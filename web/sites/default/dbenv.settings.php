e23f5f4bb447fc9e503de68b26e9a0eea0b93089
Michael Prasuhn <mike@mikeyp.net>
2016-07-11 18:18:59 -0400

    Always load DB settings from env, even when dotenv isn't used

--- web/sites/default/dbenv.settings.php
+++ web/sites/default/dbenv.settings.php
@@ -5,6 +5,7 @@
  * Integrates DB settings as parsed from environment variables.
  */
 
+// Load .env file if it exists
 if (file_exists(dirname(DRUPAL_ROOT) . '/.env')) {
   // Load environment
   $dotenv = new Dotenv\Dotenv(dirname(DRUPAL_ROOT));
@@ -12,18 +13,16 @@ if (file_exists(dirname(DRUPAL_ROOT) . '/.env')) {
 
   // Require DATABASE_URL to be defined
   $dotenv->required('DATABASE_URL')->notEmpty();
-
-  $dsn_info = parse_url(getenv('DATABASE_URL'));
-
-  $databases['default']['default'] = array (
-    'database' => ltrim($dsn_info['path'], '/'),
-    'username' => $dsn_info['user'],
-    'password' => $dsn_info['pass'],
-    'host' => $dsn_info['host'],
-    'port' => $dsn_info['port'],
-    'driver' => $dsn_info['scheme'],
-    'prefix' => '',
-    'collation' => 'utf8mb4_general_ci',
-  );
-
 }
+
+$dsn_info = parse_url(getenv('DATABASE_URL'));
+$databases['default']['default'] = array (
+  'database' => ltrim($dsn_info['path'], '/'),
+  'username' => $dsn_info['user'],
+  'password' => $dsn_info['pass'],
+  'host' => $dsn_info['host'],
+  'port' => $dsn_info['port'],
+  'driver' => $dsn_info['scheme'],
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
