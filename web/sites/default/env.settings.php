6761ffd05ea2e634b65dd9e6bd0714e91cb36027
Michael Prasuhn <mike@mikeyp.net>
2016-07-12 14:39:43 -0400

    Add development environment settings overrides

--- web/sites/default/env.settings.php
+++ web/sites/default/env.settings.php
@@ -11,3 +11,14 @@ if (file_exists(dirname(DRUPAL_ROOT) . '/.env')) {
   $dotenv = new Dotenv\Dotenv(dirname(DRUPAL_ROOT));
   $dotenv->load();
 }
+
+// Get Environment from variable or settings
+if (getenv('DRUPAL_ENV')) {
+  $settings['DRUPAL_ENV'] = getenv('DRUPAL_ENV');
+}
+if (isset($settings['DRUPAL_ENV'])) {
+  $drupal_env = $settings['DRUPAL_ENV'];
+  if (file_exists(__DIR__ . "/env/$drupal_env/settings.php")) {
+    include __DIR__ . "/env/$drupal_env/settings.php";
+  }
+}
