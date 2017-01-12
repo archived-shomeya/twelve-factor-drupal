9e3d753e14884b51a0ca07fbde91d61fadb674cc
Michael Prasuhn <mike@mikeyp.net>
2017-01-11 22:28:02 -0500

    Copy settings files instead of keeping them in vendored directory

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -20,7 +20,8 @@ class ScriptHandler {
 
   public static function createRequiredFiles(Event $event) {
     $fs = new Filesystem();
-    $root = static::getDrupalRoot(getcwd());
+    $project_root = getcwd();
+    $drupal_root = static::getDrupalRoot($project_root);
 
     $dirs = [
       'modules',
@@ -30,41 +31,63 @@ class ScriptHandler {
 
     // Required for unit testing
     foreach ($dirs as $dir) {
-      if (!$fs->exists($root . '/'. $dir)) {
-        $fs->mkdir($root . '/'. $dir);
-        $fs->touch($root . '/'. $dir . '/.gitkeep');
+      if (!$fs->exists($drupal_root . '/'. $dir)) {
+        $fs->mkdir($drupal_root . '/'. $dir);
+        $fs->touch($drupal_root . '/'. $dir . '/.gitkeep');
       }
     }
 
     // Prepare the settings file for installation
-    if (!$fs->exists($root . '/sites/default/settings.php')) {
-      $fs->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
-      $fs->chmod($root . '/sites/default/settings.php', 0666);
+    if (!$fs->exists($drupal_root . '/sites/default/settings.php')) {
+      $fs->copy($drupal_root . '/sites/default/default.settings.php', $drupal_root . '/sites/default/settings.php');
+      $fs->chmod($drupal_root . '/sites/default/settings.php', 0666);
       $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
     }
 
     // Prepare the services file for installation
-    if (!$fs->exists($root . '/sites/default/services.yml')) {
-      $fs->copy($root . '/sites/default/default.services.yml', $root . '/sites/default/services.yml');
-      $fs->chmod($root . '/sites/default/services.yml', 0666);
+    if (!$fs->exists($drupal_root . '/sites/default/services.yml')) {
+      $fs->copy($drupal_root . '/sites/default/default.services.yml', $drupal_root . '/sites/default/services.yml');
+      $fs->chmod($drupal_root . '/sites/default/services.yml', 0666);
       $event->getIO()->write("Create a sites/default/services.yml file with chmod 0666");
     }
 
     // Create the files directory with chmod 0777
-    if (!$fs->exists($root . '/sites/default/files')) {
+    if (!$fs->exists($drupal_root . '/sites/default/files')) {
       $oldmask = umask(0);
-      $fs->mkdir($root . '/sites/default/files', 0777);
+      $fs->mkdir($drupal_root . '/sites/default/files', 0777);
       umask($oldmask);
       $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
     }
 
     // Write custom config to settings.php
-    if ($fs->exists($root . '/sites/default/settings.php')) {
-      $contents = file_get_contents($root . '/sites/default/settings.php');
+    if ($fs->exists($drupal_root . '/sites/default/settings.php')) {
+      $contents = file_get_contents($drupal_root . '/sites/default/settings.php');
 
       $contents = $contents . static::getSettingsSnippet();
-      $fs->dumpFile($root . '/sites/default/settings.php', $contents, 0666);
+      $fs->dumpFile($drupal_root . '/sites/default/settings.php', $contents, 0666);
       $event->getIO()->write("Added relevant snippets to settings.php");
+    }
+
+    if (!$fs->exists($drupal_root . '/sites/default/dbenv.settings.php')) {
+      $fs->copy($project_root . '/config/settings-files/dbenv.settings.php', $drupal_root . '/sites/default/dbenv.settings.php');
+      $fs->chmod($drupal_root . '/sites/default/dbenv.settings.php', 0666);
+      $event->getIO()->write("Create a sites/default/dbenv.settings.php file with chmod 0666");
+    }
+
+    if (!$fs->exists($drupal_root . '/sites/default/env.settings.php')) {
+      $fs->copy($project_root . '/config/settings-files/env.settings.php', $drupal_root . '/sites/default/env.settings.php');
+      $fs->chmod($drupal_root . '/sites/default/env.settings.php', 0666);
+      $event->getIO()->write("Create a sites/default/env.settings.php file with chmod 0666");
+    }
+
+    if (!$fs->exists($drupal_root . '/sites/default/flysystems3.settings.php')) {
+      $fs->copy($project_root . '/config/settings-files/flysystems3.settings.php', $drupal_root . '/sites/default/flysystems3.settings.php');
+      $fs->chmod($drupal_root . '/sites/default/flysystems3.settings.php', 0666);
+      $event->getIO()->write("Create a sites/default/flysystems3.settings.php file with chmod 0666");
+    }
+
+    if (!$fs->exists($drupal_root . '/sites/default/env')) {
+      $fs->mirror($project_root . '/config/settings-files/env', $drupal_root . '/sites/default/env', NULL, ['override' => TRUE]);
     }
 
   }
